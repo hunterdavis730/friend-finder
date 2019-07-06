c205e346c1e5976b97119269f879f1a92d124e70
Hunter Davis <hunterdavis730@gmail.com>
2019-07-06 09:19:02 -0600

    added heroku db connection

--- App/Data/friends.js
+++ App/Data/friends.js
@@ -3,77 +3,74 @@ var friends = [];
 var mysql = require("mysql");
 
 function Friend(id, name, photo, scores) {
-    this.id = id;
-    this.name = name;
-    this.photo = photo;
-    this.scores = scores;
-    this.scoreArr = [];
-    this.scoreTotal = 0;
+  this.id = id;
+  this.name = name;
+  this.photo = photo;
+  this.scores = scores;
+  this.scoreArr = [];
+  this.scoreTotal = 0;
 }
 
-Friend.prototype.getArr = function () {
-    this.scoreArr = this.scores.split(", ");
+Friend.prototype.getArr = function() {
+  this.scoreArr = this.scores.split(", ");
+};
 
+Friend.prototype.calcScore = function() {
+  for (var i = 0; i < this.scoreArr.length; i++) {
+    var num = parseInt(this.scoreArr[i]);
 
-}
+    this.scoreTotal += num;
+  }
+};
 
-Friend.prototype.calcScore = function () {
-    for (var i = 0; i < this.scoreArr.length; i++) {
-        var num = parseInt(this.scoreArr[i]);
+var connection;
 
-
-        this.scoreTotal += num;
-
-    }
-}
-
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
     password: "Midwestpest1!",
     database: "friends_db"
+  });
+}
+
+connection.connect(err => {
+  if (err) throw err;
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-})
-
 function getFriends() {
-    friends = [];
-    connection.query("SELECT * FROM friends", function (err, resp) {
-        if (err) throw err;
+  friends = [];
+  connection.query("SELECT * FROM friends", function(err, resp) {
+    if (err) throw err;
 
+    var id;
+    var name;
+    var photo;
+    var scores;
+    for (var i = 0; i < resp.length; i++) {
+      id = resp[i].id;
+      name = resp[i].name;
+      photo = resp[i].photo;
+      scores = resp[i].scores;
 
-        var id;
-        var name;
-        var photo;
-        var scores;
-        for (var i = 0; i < resp.length; i++) {
-            id = resp[i].id;
-            name = resp[i].name;
-            photo = resp[i].photo;
-            scores = resp[i].scores;
+      var friend = new Friend(id, name, photo, scores);
+      friend.getArr();
+      friend.calcScore();
+      friends.push(friend);
+    }
 
-            var friend = new Friend(id, name, photo, scores)
-            friend.getArr();
-            friend.calcScore();
-            friends.push(friend)
-
-        }
-
-
-        console.log(friends)
-
-    })
-
+    console.log(friends);
+  });
 }
 getFriends();
-console.log("this " + friends)
+console.log("this " + friends);
 // module.exports = friends;
 module.exports = {
-    friends,
-    Friend,
-    connection,
-    getFriends
-}
+  friends,
+  Friend,
+  connection,
+  getFriends
+};
